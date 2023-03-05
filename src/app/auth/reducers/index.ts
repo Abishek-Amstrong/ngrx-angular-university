216b6be57fb6281e4c3cc6559aee9634a04172d7
Abishek-Amstrong <abishekamstrong@gmail.com>
2023-03-05 19:49:21 +0530

    initial commit

---
+++ src/app/auth/reducers/index.ts
@@ -0,0 +1,28 @@
+import {
+  ActionReducer,
+  ActionReducerMap,
+  createFeatureSelector,
+  createReducer,
+  createSelector,
+  MetaReducer,
+  on,
+} from "@ngrx/store";
+import { AuthActions } from "../auth-types";
+import { User } from "../model/user.model";
+
+export interface AuthState {
+  user: User;
+}
+
+export const initialState: AuthState = {
+  user: undefined,
+};
+
+export const authReducer = createReducer(
+  initialState,
+  on(AuthActions.login, (state, action) => {
+    return {
+      user: action.user,
+    };
+  })
+);
